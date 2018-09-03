27d2cc990b9a7a6872d23744976aa008eeb59fe0
Loic Alain Royer <royerloic@gmail.com>
2018-09-02 22:00:58 -0700

    big cleanup of old stuff that is never used, new build system, gradle wrapper upgrade

--- src/java/clearvolume/ClearVolumeCloseable.java
+++
@@ -1,9 +0,0 @@
-package clearvolume;
-
-import clearvolume.exceptions.ClearVolumeException;
-
-public interface ClearVolumeCloseable extends AutoCloseable
-{
-	@Override
-	void close() throws ClearVolumeException;
-}
--- src/java/clearvolume/transferf/CyclableTransferFunction.java
+++
@@ -1,8 +0,0 @@
-package clearvolume.transferf;
-
-public interface CyclableTransferFunction
-{
-	public int getCurrent();
-
-	public void next();
-}
